5acbab15504f44995218692a15c127d5d053f410
Etienne Deux <deuxetienne2@gmail.com>
2020-04-30 12:50:57 +0200

    Init

---
+++ src/test/java/isis/aced/ISISCapitalist/IsisCapitalistApplicationTests.java
@@ -0,0 +1,13 @@
+package isis.aced.ISISCapitalist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsisCapitalistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
